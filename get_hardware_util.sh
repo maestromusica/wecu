f19bfcd33f0cce0d5dc44ea94ca57695815acac1
Ubuntu <cc@hn0-cc996.dd1az33jptuuri1inzyruqvryf.jx.internal.cloudapp.net>
2020-04-04 16:14:21 +0000

    Can get hardware utilisation

--- get_hardware_util.sh
+++ get_hardware_util.sh
@@ -1,5 +1,10 @@
 
+seconds="30"
+wait_seconds="35"
+
 # Get hardware utilisation at each machine 
-parallel --sshloginfile hosts --onall --jobs 1 --will-cite "iostat -x 1 {} > usage_raw.txt" ::: "10"
-parallel --sshloginfile hosts --onall --jobs 1 --will-cite 'cat usage.txt | grep "avg-cpu" -A 1 | grep -oP "^.*?\d+.\d+\s" | grep -oP' ::: '"[\w\.]+"'
+parallel --sshloginfile hosts --nonall --jobs 1 --will-cite "rm -f usage_raw.txt usage_cpu.txt"
+parallel --sshloginfile hosts --nonall --jobs 1 --will-cite "iostat -x 1 $seconds > usage_raw.txt &"
+sleep $wait_seconds
+parallel --sshloginfile hosts --onall --jobs 1 --will-cite 'cat usage_raw.txt | grep "avg-cpu" -A 1 | grep -oP "^.*?\d+.\d+\s" | grep -oP {} > usage_cpu.txt' ::: '[\w\.]+'
 cat hosts | xargs -i scp -o "StrictHostKeyChecking no" {}:/home/cc/usage_cpu.txt ./{}.usage.txt
